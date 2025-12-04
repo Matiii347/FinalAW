70da1e75145911b98490e8d1a86fc8ce716c18b3
Matiii347 <matiasracingente@gmail.com>
2025-12-04 17:20:15 -0300

    MVC Completado

--- proyecto/modulos/api-crud/v1/proveedores/controlador.proveedores.mjs
+++ proyecto/modulos/api-crud/v1/proveedores/controlador.proveedores.mjs
@@ -1,93 +1,99 @@
-/*
-Acceso a la capa de datos - PROVEEDORES
-*/
+import * as modelo from './modelo.proveedores.mjs'
 
-import pool from '../../../../conexion/conexion.bd.mjs';
+const MENSAJES_PROVEEDOR = {
+    '23505': 'El codigo de proveedor ya existe',
+    '22001': 'La longitud del campo execede los permitidos'
+}
+const MENSAJES_TIPOS = {
+    INFO: 'info',
+    ERROR: 'error'
+}
 
-async function obtenerProveedores() {
+async function obtenerProveedores(req, res) {
     try {
-        // Ordenamos por nombre para que el listado sea prolijo
-        const resultado = await pool.query('SELECT * FROM proveedores ORDER BY nombre ASC');
-        return resultado;
+        const resultado = await modelo.obtenerProveedores();
+        if (resultado.rows.length > 0) {
+            res.json(resultado.rows);
+        } else {
+            res.status(404).json({ mensaje: 'Proveedores no encontrados', tipo: MENSAJES_TIPOS.INFO });
+        }
     } catch (error) {
         console.log(error);
-        throw error;
+        res.status(500).json({ mensaje: MENSAJES_PROVEEDOR[error.code] || 'Error en el servidor', tipo: MENSAJES_TIPOS.ERROR });
     }
 }
 
-async function obtenerProveedor(id) {
+async function obtenerProveedor(req, res) {
     try {
-        const resultado = await pool.query(
-            'SELECT * FROM proveedores WHERE id=$1',
-            [id]
-        );
-        return resultado;
+        const { id } = req.params;
+        const resultado = await modelo.obtenerProveedor(id);
+        if (resultado.rows.length > 0) {
+            res.json(resultado.rows);
+        } else {
+            res.status(404).json({ mensaje: 'Proveedor no encontrado', tipo: MENSAJES_TIPOS.INFO });
+        }
     } catch (error) {
         console.log(error);
-        throw error;
+        res.status(500).json({ mensaje: MENSAJES_PROVEEDOR[error.code] || 'Error en el servidor', tipo: MENSAJES_TIPOS.ERROR });
     }
 }
 
-async function crearProveedor(proveedor) {
+async function crearProveedor(req, res) {
     try {
-        const { nombre, telefono, email } = proveedor;
-        const resultado = await pool.query(
-            `
-            INSERT INTO proveedores
-                (nombre, telefono, email)
-            VALUES
-                ($1,$2,$3)
-            RETURNING id, nombre
-        `,
-            [nombre, telefono, email]
-        );
-        return resultado;
-    } catch (error) {
-        throw error;
-    }
-}
-
-async function modificarProveedor(proveedor) {
-    try {
-        const { id, nombre, telefono, email } = proveedor;
-        const resultado = await pool.query(
-            `UPDATE proveedores 
-                SET 
-                    nombre=$1,
-                    telefono=$2,
-                    email=$3
-                    WHERE id=$4
-                RETURNING id, nombre
-            `,
-            [nombre, telefono, email, id]
-        );
-        return resultado;
+        const { nombre, telefono, email } = req.body;
+        if (!nombre || !telefono || !email) {
+            return res.status(400).json({ mensaje: 'Datos incompletos', tipo: MENSAJES_TIPOS.ERROR });
+        }
+        const resultado = await modelo.crearProveedor({
+            nombre: nombre.trim(),
+            telefono: telefono.trim(),
+            email: email.trim(),
+        });
+        res.json({ mensaje: `Proveedor ${nombre} dado de alta`, tipo: MENSAJES_TIPOS.INFO });
     } catch (error) {
         console.log(error);
-        throw error;
+        res.status(500).json({ mensaje: MENSAJES_PROVEEDOR[error.code] || 'Error en el servidor', tipo: MENSAJES_TIPOS.ERROR });
     }
 }
 
-async function eliminarProveedor(id) {
+async function modificarProveedor(req, res) {
     try {
-        const resultado = await pool.query(
-            `DELETE FROM proveedores 
-                WHERE id=$1
-                RETURNING id, nombre
-            `,
-            [id]
-        );
-        return resultado;
+        // Asignación desestructurante SP5/H4 p.417 TID AW1
+        const { id } = req.params;
+        const { nombre, telefono, email } = req.body;
+        // Verificamos
+        if (!id || !nombre || !telefono || !email) {
+            return res.status(400).json({ mensaje: 'Datos incompletos', tipo: MENSAJES_TIPOS.ERROR });
+        }
+        const resultado = await modelo.modificarProveedor({
+            id,
+            nombre: nombre.trim(),
+            telefono: telefono.trim(),
+            email: email.trim(),
+        });
+        // { codigo: codigoModificado } -> porque codigo ya está declarada en este ámbito
+        // Ámbito SP4/H3 p.263 TID AW1
+        res.json({ mensaje: `Proveedor ${nombre} modificado`, tipo: MENSAJES_TIPOS.INFO });
     } catch (error) {
         console.log(error);
-        throw error;
+        res.status(500).json({ mensaje: MENSAJES_PROVEEDOR[error.code] || 'Error en el servidor', tipo: MENSAJES_TIPOS.ERROR });
     }
 }
 
-export {
-    obtenerProveedores,
-    obtenerProveedor,
-    crearProveedor,
-    modificarProveedor,
-    eliminarProveedor,
-};
+async function eliminarProveedor(req, res) {
+    try {
+        // Asignación desestructurante SP5/H4 p.417 TID AW1
+        const { id } = req.params;
+        const resultado = await modelo.eliminarProveedor(id);
+        if (resultado.rows.length > 0) {
+            const { nombre: nombreEliminado } = resultado.rows[0];
+            res.status(200).json({ mensaje: `Proveedor con nombre: ${nombreEliminado} eliminado`, tipo: MENSAJES_TIPOS.INFO });
+        } else {
+            res.status(404).json({ mensaje: 'Proveedor no encontrado', tipo: MENSAJES_TIPOS.ERROR });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ mensaje: MENSAJES_PROVEEDOR[error.code] || 'Error en el servidor', tipo: MENSAJES_TIPOS.ERROR });
+    }
+}
+export { obtenerProductos, obtenerProducto, crearProducto, modificarProducto, eliminarProducto };
